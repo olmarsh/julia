b2b4dce4f87dc48955ff3f93b211fcb5c580b128
olmarsh <marsh.oliver@hotmail.com>
2024-02-21 19:49:49 +1300

    added falloff
    raw values are now processed to exaggerate lower values and lower higher ones

--- main.cpp
+++ main.cpp
@@ -20,10 +20,16 @@ int main() {
     int height = 600;
 
     double re, im;
-    cout << "real, imaginary: ";
-    cin >> re >> im;
-    cout << "width, height: ";
-    cin >> width >> height;
+    double r_falloff = 15; double g_falloff = 100; double b_falloff = 500;
+    cout << "real, imaginary: "; cin >> re >> im;
+    char edit_format;
+    cout << "edit output format? y/n: "; cin >> edit_format;
+    if (edit_format == 'y' or edit_format == 'Y') {
+        cout << "width, height: ";
+        cin >> width >> height;
+        cout << "rgb falloff (lower is brighter): ";
+        cin >> r_falloff >> g_falloff >> b_falloff;
+    }
 
     vector<unsigned char> image(width*height*4);
 
@@ -31,11 +37,11 @@ int main() {
     for (int i=0;i<width;i+=1) {
     for (int j=0;j<height;j+=1) {
         // computes julia fractal for the current pixel
-        int julia_result = julia(((double)i/(double)width)*4-2,((double)j/(double)height)*4-2,re,im,radius,255);
+        double julia_result = julia(((double)i/(double)width)*4-2,((double)j/(double)height)*4-2,re,im,radius,1000);
         // writes to image vector RGBA format
-        image[4 * width * j + 4 * i + 0] = (unsigned char)julia_result;
-        image[4 * width * j + 4 * i + 1] = 0;
-        image[4 * width * j + 4 * i + 2] = 0;
+        image[4 * width * j + 4 * i + 0] = (unsigned char)((julia_result/(julia_result+r_falloff))*255);
+        image[4 * width * j + 4 * i + 1] = (unsigned char)((julia_result/(julia_result+g_falloff))*255);
+        image[4 * width * j + 4 * i + 2] = (unsigned char)((julia_result/(julia_result+b_falloff))*255);
         image[4 * width * j + 4 * i + 3] = 255;
     }
     }
